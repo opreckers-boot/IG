0db255bb1806c392e436c88d25d37c89b7761577
opreckers-boot <71862654+opreckers-boot@users.noreply.github.com>
2020-10-28 12:28:24 +0700

    Delete newfile.py

--- newfile.py
+++
@@ -1,9 +0,0 @@
-pip install instagram-explore
-import instagram_explore
-import json
-result = ie.user: nama = input("Masukkan nama IG: ")
-print ("Nama IG :", nama)
-print ("")
-parsed_data=json.dump(result,  indent = 4,
-											sort_keys = true)
-print(parsed_data[15:400])
